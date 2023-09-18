8c3c2c40999aa99a25d87f72ae56dabb6c83a0e1
Agustin Sanchez <Blueblurargnetina@gmail.com>
2023-09-18 00:29:37 -0300

    Tercera pre entrega javascript

--- main.js
+++ main.js
@@ -1,35 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botonesAgregar = document.querySelectorAll('.agregar-carrito');
+    const listaCarrito = document.getElementById('lista-carrito');
+    const totalCarrito = document.getElementById('total');
+    const btnReiniciar = document.getElementById('btn-reiniciar');
+    const btnComprar = document.getElementById('btn-comprar');
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-const ListaDeUsariosingresados = []
-let usuario;
+    botonesAgregar.forEach((boton) => {
+        boton.addEventListener('click', agregarProducto);
+    });
 
-do {
-    let NombreDeUsuario = prompt("Ingresar un nombre");
-    ListaDeUsariosingresados.push(NombreDeUsuario.toUpperCase());
-    let ApellidoDeUsuario = prompt("Ingresar un apellido");
-    ListaDeUsariosingresados.push(ApellidoDeUsuario.toUpperCase());
-    let EdadDeUsuario = prompt("indica tu edad");
-    ListaDeUsariosingresados.push(EdadDeUsuario.toUpperCase());
-    let ticket = prompt("Tienes ticket?");
-    ListaDeUsariosingresados.push(ticket.toUpperCase());
-    console.log(ListaDeUsariosingresados)
 
-    usuario = {
-        nombre: "agustin",
-        apellido: "sanchez",
-        edad: 19,
-        ticket: true,
+    btnReiniciar.addEventListener('click', reiniciarCarrito);
+    btnComprar.addEventListener('click', comprarCarrito);
+
+
+    function agregarProducto(event) {
+        const boton = event.target;
+        const nombre = boton.getAttribute('data-nombre');
+        const precio = parseFloat(boton.getAttribute('data-precio'));
+
+        const producto = {
+            nombre,
+            precio
+        };
+
+        carrito.push(producto);
+        guardarCarrito();
+        mostrarCarrito();
+        
     }
 
+    function reiniciarCarrito() {
+        carrito = [];
+        guardarCarrito();
+        mostrarCarrito();
+    }
 
-
-    if (usuario.edad > 18) {
-        alert("INGRESO CON EXITO ! El usuario es mayor de edad y tiene un ticket")
-        if (usuario.ticket === false) {
-            alert("El usuario es mayor de edad pero no tiene un ticket")
+    function comprarCarrito() {
+        if (carrito.length === 0) {
+            alert("El carrito está vacío. Agrega productos antes de comprar.");
+        } else {
+            alert("Compra realizada con éxito. Gracias por tu compra.");
+            reiniciarCarrito();
         }
     }
-    else {
-        alert("El usuario es menor de edad")
+
+
+    function guardarCarrito() {
+        localStorage.setItem('carrito', JSON.stringify(carrito));
     }
-} while (ListaDeUsariosingresados);
+
+
+    function mostrarCarrito() {
+        listaCarrito.innerHTML = '';
+        carrito.forEach((producto) => {
+            const li = document.createElement('li');
+            li.textContent = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
+            listaCarrito.appendChild(li);
+        });
+
+        const total = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+        totalCarrito.textContent = total.toFixed(2);
+    }
+});
 
